da2d2e9282ba12e349a5badbae11f26530eda93a
“YUUU23” <megan_zheng@brown.edu>
2023-03-04 15:07:50 -0500

    with Mainmenu and buttons

---
+++ Assets/Scripts/MainMenuButtonScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtonScript : MonoBehaviour
+{
+    
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("TilemapStuff");
+    }
+
+    public void GoToTutorial()
+    {
+        SceneManager.LoadScene("TutorialScreen");
+    }
+    public void GoToCredits()
+    {
+        SceneManager.LoadScene("CreditsScreen");
+    }
+
+}
+
