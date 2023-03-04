067265c9ffbe864b6c4633a4a270a057c363cf24
lena <lena_he@brown.edu>
2023-03-04 14:12:59 -0500

    Update Inventory.cs

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -13,19 +13,27 @@ public class Inventory : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
+
 
     // Update is called once per frame
     void Update()
     {
+        //calculate distance between player and selected object
         if (Input.GetMouseButtonDown(0))
         {
+            //making sure there's an actual object selected
             if(selectedObject != null)
             {
-                numBones++;
-                Destroy(selectedObject);
-                selectedObject = null;
+                float dist = Vector3.Distance(selectedObject.transform.position, transform.position);
+                //checking if the distance between player and object isn't too far
+                if(dist < maxDistance)
+                {
+                    numBones++;
+                    Destroy(selectedObject);
+                    selectedObject = null;
+                }
             }
         }
     }
