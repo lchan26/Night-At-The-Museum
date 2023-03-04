e3f15fef2b9ecccb2c5a26d8bb955c8f9544bb8a
lena <lena_he@brown.edu>
2023-03-04 13:08:53 -0500

    Update PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     float speed = 5;
 
+    private LinkedList<Vector2> keyPresses = new LinkedList<Vector2>();
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,25 +19,51 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+    
+
+
         Vector2 dir = Vector2.zero;
 
-        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey("w")) {
+        if (Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp("w")) {
             // move up
-            dir += Vector2.up;
+            removeDirection(Vector2.up);
         }
-        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey("s")) {
-            // move down
-            dir += Vector2.down;
+        if (Input.GetKeyUp(KeyCode.DownArrow) || Input.GetKeyUp("s")) {
+            removeDirection(Vector2.down);
         }
-        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey("a")) {
-            // move left
-            dir += Vector2.left;
+        if (Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp("a")) {
+            removeDirection(Vector2.left);
         }
-        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey("d")) {
-            // move right
-            dir += Vector2.right;
+        if (Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp("d")) {
+            removeDirection(Vector2.right);
         }
 
-        gameObject.GetComponent<Rigidbody2D>().velocity = speed * dir.normalized;
+        //GetKeyDown
+        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown("w")) {
+            addDirection(Vector2.up);
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown("s")) {
+            addDirection(Vector2.down);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown("a")) {
+            addDirection(Vector2.left);
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown("d")) {
+            addDirection(Vector2.right);
+        }
+
+        if (keyPresses.Count != 0) {
+        gameObject.GetComponent<Rigidbody2D>().velocity = speed * keyPresses.First.Value;
+        }
+        else gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
     }
-}
+
+    public void addDirection(Vector2 direction){
+        keyPresses.AddFirst(direction);
+    }
+
+    public void removeDirection(Vector2 direction) {
+        LinkedListNode<Vector2> target = keyPresses.Find(direction);
+        keyPresses.Remove(target);
+        }
+}       
