541c57ce73d06e4b1128df7de9ebee7ce700a64e
LoganDooley <logan_dooley@brown.edu>
2023-04-29 14:28:41 -0400

    Imported assets + added ability to go behind objects

---
+++ Assets/FlipSortingLayer.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipSortingLayer : MonoBehaviour
+{
+    private string defaultSortingLayer;
+    private int defaultSortingOrder;
+    // Start is called before the first frame update
+    void Start()
+    {
+        defaultSortingLayer = this.gameObject.GetComponent<SpriteRenderer>().sortingLayerName;
+        defaultSortingOrder = this.gameObject.GetComponent<SpriteRenderer>().sortingOrder;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        this.gameObject.GetComponent<SpriteRenderer>().sortingLayerName = "Foreground2";
+        this.gameObject.GetComponent<SpriteRenderer>().sortingOrder = 0;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        this.gameObject.GetComponent<SpriteRenderer>().sortingLayerName = defaultSortingLayer;
+        this.gameObject.GetComponent<SpriteRenderer>().sortingOrder = defaultSortingOrder;
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -39,40 +39,60 @@ public class PlayerController : MonoBehaviour
 
         Vector2 dir = Vector2.zero;
 
+        bool changedInput = false;
+
         if (Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp("w")) {
             // move up
             removeDirection(Vector2.up);
+            changedInput = true;
         }
         if (Input.GetKeyUp(KeyCode.DownArrow) || Input.GetKeyUp("s")) {
             removeDirection(Vector2.down);
+            changedInput = true;
         }
         if (Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp("a")) {
             removeDirection(Vector2.left);
+            changedInput = true;
         }
         if (Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp("d")) {
             removeDirection(Vector2.right);
+            changedInput = true;
         }
 
         //GetKeyDown
         if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown("w")) {
             addDirection(Vector2.up);
+            changedInput = true;
         }
         if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown("s")) {
             addDirection(Vector2.down);
+            changedInput = true;
         }
         if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown("a")) {
             addDirection(Vector2.left);
+            changedInput = true;
         }
         if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown("d")) {
             addDirection(Vector2.right);
+            changedInput = true;
         }
 
         if (keyPresses.Count != 0) {
+            if(gameObject.GetComponent<Rigidbody2D>().velocity == Vector2.zero && !changedInput && keyPresses.Count != 1)
+            {
+                movement = speed * keyPresses.First.Next.Value;
+                Vector2 first = keyPresses.First.Value;
+                removeDirection(first);
+                Vector2 second = keyPresses.First.Value;
+                removeDirection(second);
+                addDirection(first);
+                addDirection(second);
+            }
         movement = speed * keyPresses.First.Value;
         }
         else movement = Vector2.zero;
 
-        //gameObject.GetComponent<Rigidbody2D>().velocity = movement;
+        gameObject.GetComponent<Rigidbody2D>().velocity = movement;
     }
 
     public void addDirection(Vector2 direction){
